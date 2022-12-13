e2f7b541ca428f12fdc9376af0b8cdfc1b48976c
Aadit Tambe <aadit.tambe@gmail.com>
2022-12-12 20:13:25 -0500

    fix: put d3 code in useEffect, fix dom error

--- src/components/Sparklines.js
+++ src/components/Sparklines.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { max } from 'd3-array'
 import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection'
@@ -8,72 +8,72 @@ import { json } from 'd3-fetch';
 
 const Sparklines = props => {
     const data = "https://raw.githubusercontent.com/aadittambe/portfolio/main/gh-contribs/contribs.json"
-    json(data).then(function (json) {
-        // console.log(json);
-        var content = json.data.user.contributionsCollection.contributionCalendar.weeks
-        // console.log(content);
-        let data = {}
-        content.map(d => {
-            const startDate = d.firstDay
-            const l1 = d.contributionDays
-            // console.log(l1);
-            let weekList = 0;
-            l1.map(l => {
-                weekList = weekList + l.contributionCount
-                return weekList
+    useEffect(() => {
+        json(data).then(function (json) {
+            var content = json.data.user.contributionsCollection.contributionCalendar.weeks
+            let data = {}
+            content.map(d => {
+                const startDate = d.firstDay
+                const l1 = d.contributionDays
+                // console.log(l1);
+                let weekList = 0;
+                l1.map(l => {
+                    weekList = weekList + l.contributionCount
+                    return weekList
+                })
+                data[startDate] = weekList
+                return data
             })
-            data[startDate] = weekList
-            return data
+            data = Object.values(data).map(d => +d);
+            const width = 100;
+            const height = 20;
+            const margin = { top: 2, right: 2, bottom: 2, left: 2 };
+            const boundedwidth = width - margin.left - margin.right;
+            const boundedheight = height - margin.top - margin.bottom;
+
+            const xScale = scaleLinear()
+                .domain([0, data.length])
+                .range([0, boundedwidth]);
+            const yScale = scaleLinear()
+                .domain([0, max(data)])
+                .range([boundedheight, 0]);
+
+            const svg = select("#sparklines")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            const chartLine = line()
+                .x((d, i) => xScale(i))
+                .y((d) => yScale(d));
+
+            svg
+                .append("path")
+                .datum(data)
+                .attr("fill", "none")
+                .attr("stroke", "#f6f4e6")
+                .attr("stroke-width", 1)
+                .attr("d", chartLine);
+
+            svg
+                .append("circle")
+                .attr("r", 2)
+                .attr("cx", xScale(0))
+                .attr("cy", yScale(data[0]))
+                .attr("fill", "#f6f4e6");
+
+            svg
+                .append("circle")
+                .attr("r", 2)
+                .attr("cx", xScale(data.length - 1))
+                .attr("cy", yScale(data[data.length - 1]))
+                .attr("fill", "#edcf2e");
+            return json
         })
-        data = Object.values(data).map(d => +d);
-        // console.log(data);
-        const width = 100;
-        const height = 20;
-        const margin = { top: 2, right: 2, bottom: 2, left: 2 };
-        const boundedwidth = width - margin.left - margin.right;
-        const boundedheight = height - margin.top - margin.bottom;
+    }, [])
 
-        const xScale = scaleLinear()
-            .domain([0, data.length])
-            .range([0, boundedwidth]);
-        const yScale = scaleLinear()
-            .domain([0, max(data)])
-            .range([boundedheight, 0]);
-
-        const svg = select("#sparklines")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        const chartLine = line()
-            .x((d, i) => xScale(i))
-            .y((d) => yScale(d));
-
-        svg
-            .append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "#f6f4e6")
-            .attr("stroke-width", 1)
-            .attr("d", chartLine);
-
-        svg
-            .append("circle")
-            .attr("r", 2)
-            .attr("cx", xScale(0))
-            .attr("cy", yScale(data[0]))
-            .attr("fill", "#f6f4e6");
-
-        svg
-            .append("circle")
-            .attr("r", 2)
-            .attr("cx", xScale(data.length - 1))
-            .attr("cy", yScale(data[data.length - 1]))
-            .attr("fill", "#edcf2e");
-        return json
-    })
     return (<span id='sparklines'></span>)
 }
 
