7195d231642ea1cfabc6e62b47d2078274dbd814
Aadit Tambe <aadit.tambe@gmail.com>
2023-01-12 23:14:59 -0500

    fix: base

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import '../styles/_base.scss'
+import '../styles/base.scss'
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 const data = require('../data/content.json')
