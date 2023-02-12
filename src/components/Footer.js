2472b1de3b4892dae252da6458b53a9406a87585
Aadit Tambe <aadit.tambe@gmail.com>
2023-02-12 15:40:18 -0500

    feat: new footer languate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ const Footer = props => {
 
         <footer className='footer'>
             <hr></hr>
-            <p>© Handmade by Aadit Tambe | 2019 – 2023
+            <p>© Site developed by Aadit Tambe with Next.js, Google Docs, and GitHub Actions — <span className="source ital"><a href='https://github.com/aadittambe/aadittambe.com'>view source</a></span> | 2019 – 2023
+
             </p>
 
         </footer>
