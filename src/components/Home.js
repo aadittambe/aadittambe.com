5a5c391691b784890a046bddceca68a21d2550f6
Aadit Tambe <aadit.tambe@gmail.com>
2023-10-10 00:40:08 +0900

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,54 +13,56 @@ const Home = props => {
 
 
     return <main className="home">
-        <h1><span className='wave'>👋🏽</span> Hi, I am Aadit!</h1>
-        <p><span style={{ display: "none" }}>👨🏽‍💻</span>I tell stories — but with<span className='mob-jump'><br /></span> <span className="typeit">
-            <TypeIt
-                options={{
-                    loop: true,
-                    speed: 200,
-                    waitUntilVisible: true,
-                    lifeLike: true,
-                }}
-                getBeforeInit={(instance) => {
-                    instance
-                        .pause(1500)
-                        .type("code. 🖥")
-                        .pause(2000)
-                        .delete(7)
-                        .type("graphics. 📊")
-                        .pause(2500)
-                        .delete(11)
-                        .type("design. 🎨")
-                        .pause(2700)
-                        .delete(9)
-                        .type("data. 📈")
-                        .pause(2100);
-
-                    return instance;
-                }}
-            /></span></p>
-
-        {intro.map((text, index) => (
-            index === 1 ? <p key={index}>{text} <Sparklines />.</p> :
-                <p key={index}>{text}</p>
-        ))}
-        <a href='https://vis.social/@aadittambe' rel="noreferrer" target="_blank">
-            <FontAwesomeIcon className="icon" icon={faMastodon} size='2x' />
-        </a>
-        <a href='https://twitter.com/aadittambe/' rel="noreferrer" target="_blank">
-            <FontAwesomeIcon className="icon" icon={faTwitter} size='2x' />
-        </a>
-        <a href='https://github.com/aadittambe' rel="noreferrer" target="_blank">
-            <FontAwesomeIcon className="icon" icon={faGithubAlt} size='2x' />
-        </a>
-        <a href='https://www.linkedin.com/in/aadittambe/' rel="noreferrer" target="_blank">
-            <FontAwesomeIcon className="icon" icon={faLinkedin} size='2x' />
-        </a>
-        <a href='mailto:aadit.tambe@gmail.com' rel="noreferrer" target="_blank">
-            <FontAwesomeIcon className="icon" icon={faEnvelope} size='2x' />
-        </a>
+       
     </main>
 }
 
 export default Home
+
+//  <h1><span className='wave'>👋🏽</span> Hi, I am Aadit!</h1>
+//         <p><span style={{ display: "none" }}>👨🏽‍💻</span>I tell stories — but with<span className='mob-jump'><br /></span> <span className="typeit">
+//             <TypeIt
+//                 options={{
+//                     loop: true,
+//                     speed: 200,
+//                     waitUntilVisible: true,
+//                     lifeLike: true,
+//                 }}
+//                 getBeforeInit={(instance) => {
+//                     instance
+//                         .pause(1500)
+//                         .type("code. 🖥")
+//                         .pause(2000)
+//                         .delete(7)
+//                         .type("graphics. 📊")
+//                         .pause(2500)
+//                         .delete(11)
+//                         .type("design. 🎨")
+//                         .pause(2700)
+//                         .delete(9)
+//                         .type("data. 📈")
+//                         .pause(2100);
+
+//                     return instance;
+//                 }}
+//             /></span></p>
+
+//         {intro.map((text, index) => (
+//             index === 1 ? <p key={index}>{text} <Sparklines />.</p> :
+//                 <p key={index}>{text}</p>
+//         ))}
+//         <a href='https://vis.social/@aadittambe' rel="noreferrer" target="_blank">
+//             <FontAwesomeIcon className="icon" icon={faMastodon} size='2x' />
+//         </a>
+//         <a href='https://twitter.com/aadittambe/' rel="noreferrer" target="_blank">
+//             <FontAwesomeIcon className="icon" icon={faTwitter} size='2x' />
+//         </a>
+//         <a href='https://github.com/aadittambe' rel="noreferrer" target="_blank">
+//             <FontAwesomeIcon className="icon" icon={faGithubAlt} size='2x' />
+//         </a>
+//         <a href='https://www.linkedin.com/in/aadittambe/' rel="noreferrer" target="_blank">
+//             <FontAwesomeIcon className="icon" icon={faLinkedin} size='2x' />
+//         </a>
+//         <a href='mailto:aadit.tambe@gmail.com' rel="noreferrer" target="_blank">
+//             <FontAwesomeIcon className="icon" icon={faEnvelope} size='2x' />
+//         </a>
