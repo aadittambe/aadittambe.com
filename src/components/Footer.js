c816202a174e4c01166cd0ec67218d1cd4c16bdc
Aadit Tambe <aadit.tambe@gmail.com>
2024-01-08 16:03:41 +0900

    update year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-const Footer = props => {
-    return (
-
-        <footer className='footer'>
-            <hr></hr>
-            <p>© Site developed by Aadit Tambe with Next.js, Google Docs, and GitHub Actions — <span className="source ital"><a href='https://github.com/aadittambe/aadittambe.com'>view source</a></span> | 2019 – 2023
-
-            </p>
-
-        </footer>
-    )
-}
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <hr></hr>
+      <p>
+        © Site developed by Aadit Tambe with Next.js, Google Docs, and GitHub
+        Actions —{" "}
+        <span className="source ital">
+          <a href="https://github.com/aadittambe/aadittambe.com">view source</a>
+        </span>{" "}
+        | 2019 – {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
