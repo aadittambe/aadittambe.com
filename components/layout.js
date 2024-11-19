e4227c9aecbb0ceb99d8026f6bccdad7912f172a
Aadit Tambe <aadit.tambe@gmail.com>
2024-11-19 23:04:40 +0900

    update property

--- components/layout.js
+++ components/layout.js
@@ -18,7 +18,7 @@ export default function Layout({ children, home, blog }) {
           content="Aadit Tambe is a journalist and developer who tells data-driven stories visually — with code."
         />
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/logo592.png" />
         <link rel="shortcut icon" href="/static/logo592.png" />
