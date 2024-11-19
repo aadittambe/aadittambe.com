86a273f2c16a2502ff35a5bbecbaa13fd7d1ecb2
Aadit Tambe <aadit.tambe@gmail.com>
2024-11-19 23:16:48 +0900

    updating deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,29 @@
 const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
-require('dotenv').config();
+require("dotenv").config();
 
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-
 const config = {
-    user: process.env.FTP_USERNAME,
-    password: process.env.FTP_PASSWORD,
-    host: "aadittambe.com",
-    port: 21, // Your credentials
+  user: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 22, // Your credentials
 
-    localRoot: 'out/', // Location of build files in project
-    remoteRoot: "public_html/", // Upload location on remote, replace with subfolder on FTP-server if required
+  localRoot: "out/", // Location of build files in project
+  remoteRoot: "public_html/", // Upload location on remote, replace with subfolder on FTP-server if required
 
-    include: ["*", "**/*"], // Upload all files from build folder
-    exclude: [], // Exclude no files
+  include: ["*", "**/*"], // Upload all files from build folder
+  exclude: [], // Exclude no files
 
-    deleteRemote: false, // Set to true if you want to delete ALL FILES in the remote root before uploading
-    forcePasv: true // Use passive mode
+  deleteRemote: true, // Set to true if you want to delete ALL FILES in the remote root before uploading
+  forcePasv: true, // Use passive mode
+  sftp: true,
 };
 
 ftpDeploy
-    .deploy(config)
-    .then((res) => console.log("finished:", res))
-    .catch((err) => console.log(err));
+  .deploy(config)
+  .then((res) => console.log("finished:", res))
+  .catch((err) => console.log(err));
