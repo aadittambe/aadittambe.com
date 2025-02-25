7800b890499c38bc5d5e34ebba0d723a8df526c1
Aadit Tambe <aadit.tambe@gmail.com>
2025-02-25 15:22:10 +0900

    use layout for posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -6,47 +6,55 @@ import {
 } from "../../lib/posts";
 import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
+
 const longAP = require("ap-style-date").longAP;
 
 export default function Post({ postData, prevPost, nextPost }) {
   return (
-    <div className="post">
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
+    <Layout>
+      <div className="post">
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
 
-      <main>
-        <article>
-          <h1>{postData.title}</h1>
-          <p>{postData.subhead}</p>
-          <p>{longAP(postData.date)}</p>
-          <div className="divider" />
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-        <nav>
-          <div className="actions">
-            {prevPost && (
-              <>
-                <p className="label">← Previous</p>
-                <p>
-                  <Link href={`/blog/${prevPost.slug}`}>{prevPost.title}</Link>
-                </p>
-              </>
-            )}
-          </div>
-          <div className="actions next">
-            {nextPost && (
-              <>
-                <p className="label">Next →</p>
-                <p>
-                  <Link href={`/blog/${nextPost.slug}`}>{nextPost.title}</Link>
-                </p>
-              </>
-            )}
-          </div>
-        </nav>
-      </main>
-    </div>
+        <main>
+          <article>
+            <h1>{postData.title}</h1>
+            <p>{postData.subhead}</p>
+            <p>{longAP(postData.date)}</p>
+            <div className="divider" />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </article>
+          <nav>
+            <div className="actions">
+              {prevPost && (
+                <>
+                  <p className="label">← Previous</p>
+                  <p>
+                    <Link href={`/blog/${prevPost.slug}`}>
+                      {prevPost.title}
+                    </Link>
+                  </p>
+                </>
+              )}
+            </div>
+            <div className="actions next">
+              {nextPost && (
+                <>
+                  <p className="label">Next →</p>
+                  <p>
+                    <Link href={`/blog/${nextPost.slug}`}>
+                      {nextPost.title}
+                    </Link>
+                  </p>
+                </>
+              )}
+            </div>
+          </nav>
+        </main>
+      </div>
+    </Layout>
   );
 }
 
