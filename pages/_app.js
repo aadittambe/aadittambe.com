d61dfbdfc94038ed5aa7c02669d7a4be10290ea1
Aadit Tambe <aadit.tambe@gmail.com>
2025-02-25 15:21:59 +0900

    rm cl

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,5 @@ import "../styles/base.scss";
 import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }) {
-  console.log(pageProps);
-
   return <Component {...pageProps} />;
 }
