fb5e5882823092ee2e2394eb5ab1189acb50d54e
Aadit Tambe <aadit.tambe@gmail.com>
2025-02-28 17:26:03 +0900

    adding a tooltip

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "../components/layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "react-tooltip";
 
 const ContactPage = () => {
   const socials = [
@@ -21,22 +24,27 @@ const ContactPage = () => {
       link: "https://github.com/aadittambe/",
     },
     {
-      name: "Mastodon",
-      text: "vis.social/aadittambe",
-      link: "https://vis.social/@aadittambe",
-      inActive: true,
-    },
-    {
       name: "Bluesky",
       text: "aadittambe.bsky.social",
       link: "https://bsky.app/profile/aadittambe.bsky.social",
       inActive: true,
+      tooltipText:
+        "I occasionally check Bluesky because it’s where most of the developer-journalists I know hang out, but rarely post.",
+    },
+    {
+      name: "Mastodon",
+      text: "vis.social/aadittambe",
+      link: "https://vis.social/@aadittambe",
+      inActive: true,
+      tooltipText: "I do not actively monitor Mastodon.",
     },
     {
       name: "Twitter",
       text: "aadittambe",
       link: "https://twitter.com/aadittambe",
       inActive: true,
+      tooltipText:
+        "My account is up, although I avoid checking Twitter. But during breaking-news events, it can’t be helped.",
     },
   ];
 
@@ -51,18 +59,33 @@ const ContactPage = () => {
         {socials.map((s, i) => (
           <div key={i} className="flex">
             <p className="left">{s.name}</p>
-            <p
-              className="right"
-              style={{
-                textDecoration:
-                  s.inActive &&
-                  "line-through solid rgba(48, 56, 65, 0.6) 1.5px",
-              }}
-            >
+            <p className="right">
               {s.link ? (
-                <a href={s.link} target="_blank">
-                  {s.text}
-                </a>
+                <>
+                  <a href={s.link} target="_blank">
+                    {s.text}
+                  </a>
+                  {s.inActive && (
+                    <>
+                      <FontAwesomeIcon
+                        icon={faCircleInfo}
+                        data-tooltip-id={`${s.name}-tooltip`}
+                        data-tooltip-content={s.tooltipText}
+                        data-tooltip-place="right"
+                        style={{
+                          width: "20px",
+                          verticalAlign: "middle",
+                          marginLeft: "12px",
+                        }}
+                      />
+                      <Tooltip
+                        className="tooltip"
+                        id={`${s.name}-tooltip`}
+                        place="right"
+                      />
+                    </>
+                  )}
+                </>
               ) : (
                 s.text
               )}
